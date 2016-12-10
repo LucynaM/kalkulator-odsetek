9ba0c023199be631aa49f5fac3c8e0b29d90c6c4
Lucyna Mazur <lucyna@studiokreator.com>
2016-12-10 15:06:06 +0100

    final

--- js/calculator.js
+++ js/calculator.js
@@ -2,25 +2,28 @@
 
 function calculator() {
 	
-	var ko = document.getElementById("wklad").value;
+	var ko = parseFloat(document.getElementById("wklad").value);
 	console.log("wklad: " + ko);
 	
 	var e = document.getElementById("kapitalizacja")
 	var m = e.options[e.selectedIndex].value;
 	console.log("okres kapitalizacji: " + m);
 	
-	var r = (document.getElementById("stopa").value)/100;
+	var r = parseFloat((document.getElementById("stopa").value)/100);
 	console.log("roczna stopa zwrotu: " + r);
 	
-	var n = document.getElementById("lata").value;
+	var n = parseInt(document.getElementById("lata").value);
 	console.log("liczba lat: " + n);
 	
 	var K = ko*(Math.pow((1+r/m), (n*m) ));	
 	if (document.getElementById("belka").checked) {
 		K -= (K-ko)*(0.19);
 	} 
+	
 	console.log(K);
- 	document.getElementById("wynik").innerHTML = Math.round(K*100)/100;
+ 	document.getElementById("wynik").innerHTML = Math.round(K*100)/100;	
+	
+	/*document.getElementById("wynik").innerHTML = parseFloat(K).toFixed(2); wersja alternatywna do 100*/
 	
 }
 				 
